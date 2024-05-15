01b0ea024c2c7d364b5ddf1ba8ac602c56be5764
Amit Verma <77237968+amit862@users.noreply.github.com>
2024-05-15 20:45:42 +0530

    Update TOH.java

--- TOH.java
+++ TOH.java
@@ -8,6 +8,7 @@ public class TOH {
         }
 
         towerOfHanoi(n - 1, src, dest, helper);
+
         System.out.println("transfer disk " + n + " from " + src + " to " + dest);
         towerOfHanoi(n - 1, helper, src, dest);
 
